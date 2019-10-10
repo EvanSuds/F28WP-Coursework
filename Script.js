1eadb61ef6ff4abc8dfc3cde021febce30fea128
Evan Sutherland <evan_suds@yahoo.com>
2019-10-10 13:19:22 +0100

    Added username form and input validation

--- Script.js
+++ Script.js
@@ -1,1 +1,7 @@
-
+function formValidation() {
+  var x = document.forms["usernameForm"]["Username"].value;
+  if (x.length < 3) {
+    alert("Username must be at least 3 letters long");
+    return false;
+  }
+}
