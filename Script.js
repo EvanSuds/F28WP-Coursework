e560fe332e9c1ea7ec85914b831ea7af1f44251c
Evan Sutherland <evan_suds@yahoo.com>
2019-10-11 10:35:26 +0100

    Changed form validation to have an upper limit

--- Script.js
+++ Script.js
@@ -1,7 +1,7 @@
 function formValidation() {
   var x = document.forms["usernameForm"]["Username"].value;
-  if (x.length < 3) {
-    alert("Username must be at least 3 letters long");
+  if (x.length < 3 || x.length > 12) {
+    alert("Username must be between 3 and 12 letters long");
     return false;
   }
 }
