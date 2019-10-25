55e6b5f15998a4ac5c19fd7f1c926797b1c08a4d
LewisI224 <incheslewis@gmail.com>
2019-10-25 11:32:07 +0100

    altered position of gun

--- game.js
+++ game.js
@@ -84,7 +84,7 @@ function tank() {
   ctx.translate(this.xpos, this.ypos);
   ctx.rotate(this.angle);
   ctx.drawImage(document.getElementById("tankBody"), 0, 0, this.width*1.5, this.height*1.5, -this.width/2, -this.height/2, this.width, this.height);
-  ctx.drawImage(document.getElementById("tankArm"), 35 ,- 30 ,50, 100,-25,-50,50,100);
+  ctx.drawImage(document.getElementById("tankArm"), 20 ,- 15 ,50, 100,-25,-50,50,100);
   ctx.restore();
   }
 
