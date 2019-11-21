5575e937e645871a3cbc3b424bdb6dfdee4d0304
EvanSuds <ess5@hw.ac.uk>
2019-11-21 22:11:10 +0000

    Update index.html

--- server.js
+++ server.js
@@ -10,6 +10,24 @@ app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname, 'index.html'));
   
 });// Starts the server.
+
+//TODO, Get login screen to work
+/*
+app.get('/', function(req, res){
+  res.render('form');// if jade
+  // You should use one of line depending on type of frontend you are with
+  res.sendFile(__dirname + '/form.html'); //if html file is root directory
+ res.sendFile("game.html"); //if html file is within public directory
+});
+
+app.post('/',function(req,res){
+   var username = req.body.username;
+   var htmlData = 'Hello:' + username;
+   res.send(htmlData);
+   console.log(htmlData);
+
+});
+*/
 server.listen(5000, function() {
   console.log('Starting server on port 5000');
 });
