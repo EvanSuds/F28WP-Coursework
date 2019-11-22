d7d2da86a7f7e047379dbe0cc114d96b60bb70b9
EvanSuds <ess5@hw.ac.uk>
2019-11-22 15:11:43 +0000

    Remove mouse positions and angle function

--- public/javascript/mousepositioning.js
+++ public/javascript/mousepositioning.js
@@ -1,30 +1,4 @@
 document.addEventListener("mousedown",leftClick);
-
-
 function leftClick(e) {
   playerShell.fire(e);
-
-}
-function mouseposition(canvas,e) {
-  /*
-  var canvasOffset=$("canvas").offset();
-  var offsetX = canvasOffset.left;
-  var offsetY = canvasOffset.top;
-  var x = parseInt(e.clientX-offsetX);
-  var y = parseInt(e.clientY-offsetY);
-  return {x: x, y: y};
-  */
-
-  var rect = canvas.getBoundingClientRect();
-        return {
-          x: evt.clientX - rect.left,
-          y: evt.clientY - rect.top
-        };
-}
-
-
-function angleBetweenPoints(mousepos, tankpos) {
-  console.log("Mouse x position: " + mousepos.x + " Mouse y position: " + mousepos.y);
-  console.log("Tank x position: " + tankpos.x + " Tank y position: " + tankpos.y);
-  return Math.atan2(mousepos.y - tankpos.y, mousepos.x - tankpos.x);
 }
