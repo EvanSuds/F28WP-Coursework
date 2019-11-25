80f70c2b61002f7c3bf225df3ee223906adad853
ASmithie <44504052+ASmithie@users.noreply.github.com>
2019-11-25 04:03:16 +0000

    More SignUp

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,4 +1,5 @@
 // JavaScript source code
 function SignUp() {
-    
+    socket.emit('newUser', {name: document.getElementById("userName").value, pwd: document.getElementById.getElementById("userPW").value});
+    alert("You have officially signed up");
 }
