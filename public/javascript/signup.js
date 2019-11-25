4b841362ea346d9850d1f20e3de3324ab507d84d
Daniel Townson <danieltownson4@gmail.com>
2019-11-25 04:49:08 +0000

    Removed unnecessary code

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -4,10 +4,6 @@ function SignUp() {
 
     var socket = io();
 
-    socket.on('message', function(data) { //Receive a text message from the server
-      console.log(data); //Log ("Communicating with the server!")
-    });
-
     socket.emit('newUser', {name: document.getElementById("userName").value, pws: document.getElementById("userPW").value});
     alert("You have officially signed up");
 }
