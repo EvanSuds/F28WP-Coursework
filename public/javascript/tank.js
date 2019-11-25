37ae512e88a9c7a0e34b0ce14633f36853389e28
LewisI224 <incheslewis@gmail.com>
2019-11-25 02:33:58 +0000

    comments

--- public/javascript/tank.js
+++ public/javascript/tank.js
@@ -9,13 +9,13 @@ function tank() {
    //Function to update the tank on the canvas
      this.update = function() {
      window.ctx = gameCanvas.context;
-     ctx.save();
-     ctx.translate(this.xpos, this.ypos);
-     ctx.rotate(this.angle);
-     ctx.drawImage(document.getElementById("tankBody"),-this.width/2,-this.height/2,this.width,this.height);
-     ctx.restore();
+     ctx.save(); //saves the state of the game canvas
+     ctx.translate(this.xpos, this.ypos); //changes the origin of the canvas to the center of the tank
+     ctx.rotate(this.angle); //rotates the entire canvas by the desired angle
+     ctx.drawImage(document.getElementById("tankBody"),-this.width/2,-this.height/2,this.width,this.height); //redraw the tank on the rotated canvas pointing upwards
+     ctx.restore(); //restore the canvas original rotation therefore also rotating the tank to the desired position
      }
-//495*477
+
      this.kill = function() { //Called if the player kills another player
        playerTank.score += 100;
        playerTank.totalKilled++;
